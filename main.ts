faff5f5b8e469b4f1a0d7466186e3002aca26cc1
seoyeonpyo <117896485+seoyeonpyo@users.noreply.github.com>
2022-12-06 23:37:00 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -2,14 +2,14 @@ radio.onReceivedString(function (receivedString) {
     if (receivedString == "lightsON") {
         네모_neo.showColor(neopixel.colors(NeoPixelColors.Black))
         for (let index = 0; index <= 48; index++) {
-            원형_neo.setPixelColor(index, neopixel.colors(NeoPixelColors.White))
+            원형_neo.setPixelColor(index, neopixel.colors(NeoPixelColors.Orange))
             원형_neo.show()
             basic.pause(40)
         }
     }
     if (receivedString == "colorON") {
         for (let index = 0; index <= 48; index++) {
-            원형_neo.setPixelColor(index, neopixel.colors(NeoPixelColors.Orange))
+            원형_neo.setPixelColor(index, neopixel.colors(NeoPixelColors.Purple))
             원형_neo.show()
             basic.pause(100)
         }
@@ -23,10 +23,8 @@ let 원형_neo: neopixel.Strip = null
 radio.setGroup(77)
 radio.setTransmitPower(7)
 원형_neo = neopixel.create(DigitalPin.P13, 45, NeoPixelMode.RGB)
-원형_neo.setBrightness(20)
 원형_neo.showColor(neopixel.colors(NeoPixelColors.Black))
 네모_neo = neopixel.create(DigitalPin.P16, 64, NeoPixelMode.RGB)
-네모_neo.setBrightness(20)
 let range_off_1 = 네모_neo.range(1, 15)
 let range_off_2 = 네모_neo.range(48, 63)
 let range_on = 네모_neo.range(16, 47)
